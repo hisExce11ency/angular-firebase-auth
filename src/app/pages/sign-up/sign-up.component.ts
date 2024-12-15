88170d8b14408d93927e97875dcd12634e106cf3
hisExce11ency <157422766+hisExce11ency@users.noreply.github.com>
2024-12-15 17:18:20 +0200

    Implement cross field Validation  and simple reactive form Validations to the Sign Up Form

--- src/app/pages/sign-up/sign-up.component.ts
+++ src/app/pages/sign-up/sign-up.component.ts
@@ -1,9 +1,31 @@
 import { Component, inject } from '@angular/core';
-import { FormBuilder, ReactiveFormsModule } from '@angular/forms';
+import {
+  AbstractControl,
+  FormBuilder,
+  ReactiveFormsModule,
+  ValidationErrors,
+  ValidatorFn,
+  Validators,
+} from '@angular/forms';
 
 import { MatButtonModule } from '@angular/material/button';
 import { MatFormFieldModule } from '@angular/material/form-field';
 import { MatInputModule } from '@angular/material/input';
+
+export function passwordsMatchValidator(): ValidatorFn {
+  return (control: AbstractControl): ValidationErrors | null => {
+    const password = control.get('password')?.value;
+    const confirmPassword = control.get('confirmPassword')?.value;
+
+    if (password && confirmPassword && password !== confirmPassword) {
+      return {
+        passwordsDontMatch: true,
+      };
+    } else {
+      return null;
+    }
+  };
+}
 
 @Component({
   selector: 'app-sign-up',
@@ -20,12 +42,15 @@ import { MatInputModule } from '@angular/material/input';
 export class SignUpComponent {
   fb = inject(FormBuilder);
 
-  signUpForm = this.fb.group({
-    name: [''],
-    email: [''],
-    password: [''],
-    confirmPassword: [''],
-  });
+  signUpForm = this.fb.group(
+    {
+      name: ['', Validators.required],
+      email: ['', [Validators.required, Validators.email]],
+      password: ['', Validators.required],
+      confirmPassword: ['', Validators.required],
+    },
+    { validators: passwordsMatchValidator() }
+  );
 
   submit() {
     console.log(this.signUpForm.value);
